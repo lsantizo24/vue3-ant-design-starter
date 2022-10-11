de59452fb8bc3ae7facb59000c76b69b09c51739
Neri <neri.ramirez549@gmail.com>
2022-10-10 17:43:58 -0700

    Added padding to lightbox in image-inf component

--- src/components/image-inf.vue
+++ src/components/image-inf.vue
@@ -1,33 +1,38 @@
 <template>
-   <a-image 
-  height="default" 
-  width="default" 
-  src="https://tahoejacks-vnhqjfv0atvxcid.netdna-ssl.com/wp-content/uploads/2015/08/Ridgeline-Express-Tahoe-hiking.jpg"
-  />
-<div class="abs"><span>Cuotas
-<router-link to="/event-details">Reservar Ahora</router-link>
-</span> 
-</div>
+  <div class="img">
+    <a-image
+      height="default"
+      width="default"
+      src="https://tahoejacks-vnhqjfv0atvxcid.netdna-ssl.com/wp-content/uploads/2015/08/Ridgeline-Express-Tahoe-hiking.jpg"
+    />
+  </div>
+  <div class="abs">
+    <span
+      >Cuotas
+      <router-link to="/event-details">Reservar Ahora</router-link>
+    </span>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 export default defineComponent({});
 </script>
 
 <style>
 .abs {
-position: absolute;
-background-color: lightblue;
-width: 300px;
-height: 120px;
-top: 600px;
-right: 0;
-border: 2px solid rgb(252, 252, 252);
-color: rgb(5, 0, 0);
-font-size: 25px;
+  position: absolute;
+  background-color: lightblue;
+  width: 300px;
+  height: 120px;
+  top: 600px;
+  right: 0;
+  border: 2px solid rgb(252, 252, 252);
+  color: rgb(5, 0, 0);
+  font-size: 25px;
+}
+
+.ant-image-preview-img-wrapper{
+  padding: 4%;
 }
 </style>
-
-
-
